9e0f6a736baf6040e62d1d04d851b30e1a1452d2
Gerardo A. Rivera Tello <gerardo_art@me.com>
2017-05-27 03:41:22 -0500

    Update plot function to use Basemap

--- NETCDF scripts/SODA3/anomalies.py
+++ NETCDF scripts/SODA3/anomalies.py
@@ -3,28 +3,41 @@
 """
 Created on Wed May 24 15:40:25 2017
 
-@author: DangoMelon0701
+@author: Gerardo A. Rivera Tello
 """
+from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
-import matplotlib as mpl
+from scipy.io import netcdf
 from osgeo import gdal,osr
 import numpy as np
 import os
+#%%
+if 'GDAL_DATA' not in os.environ:
+    os.environ['GDAL_DATA'] = r'/home/DangoMelon0701/anaconda3/pkgs/libgdal-2.1.0-0/share/gdal'
 
 #%%
-#Funcion de ayuda para visualizar los datos trabajados
-def plot_data(data,cbar=0,save_img=0,name='image',norm = 0):
-    plot,axs = plt.subplots()
-    if norm == 1:
-        norm = mpl.colors.Normalize(vmin=-0.5, vmax=0.5)
-        cmap = mpl.cm.get_cmap('jet')
-        raw_data = axs.imshow(data,interpolation="gaussian",cmap=cmap,norm=norm)
+# Funcion para plot en el mapa
+def plot_map(data,lat,lon,band=None,title=None,savefig=None,name='image'):
+    fig, axis = plt.subplots(figsize=(10,20))
+    m = Basemap(projection = 'cyl', resolution = 'l',
+                llcrnrlat=lat.min()-1,urcrnrlat=lat.max()+1,
+                llcrnrlon=lon.min()-1, urcrnrlon=lon.max()+1)
+    m.drawcoastlines(linewidth = 0.5)
+    m.drawcountries()
+    m.drawparallels(np.arange(-90.0,90.0,2.0), labels = [1,0,0,0])
+    m.drawmeridians(np.arange(-180.0,180.0,2.0), labels = [0,0,0,0],linewidth=0.5)
+    m.drawmeridians(np.arange(-180.0,180.0,10.0), labels = [0,0,0,1],linewidth=0.5)
+    x, y =m(lon, lat)
+    if band == None:
+        mmap=m.pcolormesh(x, y, data, vmin=data.min(),vmax=data.max(),cmap=plt.cm.bwr)
     else:
-        raw_data = axs.imshow(data,interpolation="gaussian",cmap='jet')
-    if cbar == 1:
-        cbar = plot.colorbar(raw_data)
-    if save_img == 1:
-        plt.savefig("{}.png".format(name),dpi=1000,bbox_inches='tight')
+        mmap=m.pcolormesh(x, y, data[band], vmin=data.min(),vmax=data.max(),cmap=plt.cm.bwr)
+    cbar = m.colorbar(mmap,location='bottom',size='10%',pad='15%')
+    cbar.set_label('°C')
+    if title != None:
+        axis.set_title(title)
+    if savefig != None:
+        fig.savefig("{}.png".format(name),dpi=1000,bbox_inches='tight')
 
 #%%
 #Esta clase calcula las anomalias del siguiente conjunto de ecuaciones
@@ -34,14 +47,17 @@ def plot_data(data,cbar=0,save_img=0,name='image',norm = 0):
 
 class Anomalies(object):
     
-    def __init__(self,gdal_netcdf):
-        self.gdal_file = gdal.Open(gdal_netcdf)
-        self.bandsn = self.gdal_file.RasterCount
-        self.lines = self.gdal_file.RasterYSize
-        self.samples = self.gdal_file.RasterXSize
+    def __init__(self,data_netcdf):
+        self.nc_file = netcdf.NetCDFFile(data_netcdf,'r')
+        self.bandsn = self.nc_file.dimensions['time']
+        self.lines = self.nc_file.dimensions['latitude']
+        self.samples = self.nc_file.dimensions['longitude']
         self.whole_data = np.zeros([self.bandsn,self.lines,self.samples])
+        self.lon,self.lat = np.meshgrid(self.nc_file.variables['longitude'][:],
+                                        self.nc_file.variables['latitude'][:]) 
+        self.temp = self.nc_file.variables['temp'][:,0]
         for band in range(1,self.bandsn+1):
-            self.whole_data[band-1]=self.gdal_file.GetRasterBand(band).ReadAsArray()
+            self.whole_data[band-1]=self.temp[band-1]
         
     def get_whole_mean(self):
         data_mean = np.zeros([self.lines,self.samples])
@@ -71,11 +87,18 @@ class Anomalies(object):
             anomalie[12*(year-1):12*year]=self.whole_data[12*(year-1):12*year]-climval
         return anomalie
     
-    def save_tiff(self,data,raster_out="stacked_data.tif"):
+    def get_anomalie_mean(self,anomalie):
+        anomalie_mean = np.zeros([self.lines,self.samples])
+        for band in range(self.bandsn):
+            anomalie_mean +=anomalie[band]
+        return anomalie_mean/self.bandsn
+    
+    def save_tiff(self,file,data,raster_out="stacked_data.tif"):
         #Genero el tiff de salida
         driver = gdal.GetDriverByName('GTiff')
         outRaster = driver.Create(raster_out, self.samples, self.lines, self.bandsn, gdal.GDT_Float32)
-        outRaster.SetGeoTransform(self.gdal_file.GetGeoTransform())
+        gdal_file = gdal.Open(file)
+        outRaster.SetGeoTransform(gdal_file.GetGeoTransform())
         for band in range(1,self.bandsn+1):
             outband = outRaster.GetRasterBand(band)
             outband.WriteArray(data[band-1])
@@ -83,10 +106,10 @@ class Anomalies(object):
         outRasterSRS.ImportFromEPSG(4326)
         outRaster.SetProjection(outRasterSRS.ExportToWkt())
         outband.FlushCache()
-        del outRaster, outRasterSRS
+        del outRaster, outRasterSRS, gdal_file
     
     def end_anom(self):
-        del self.gdal_file
+        self.nc_file.close()
 #%%
 
 if __name__ == '__main__':
@@ -94,7 +117,19 @@ if __name__ == '__main__':
     for files in os.listdir(os.getcwd()):
         if files.endswith(".nc"):
             files_list.append(files)
-    for netcdf in files_list:
-        calc = Anomalies(netcdf)
-        calc.save_tiff(calc.get_anomalie(),raster_out='ptemp_stacked.tif')
+    for f_netcdf in files_list:
+        calc = Anomalies(f_netcdf)
+        anom = calc.get_anomalie()
+        anom_mean = calc.get_anomalie_mean(anom)
+        calc.save_tiff(f_netcdf,anom,raster_out='ptemp_stacked.tif')
+#%%
+        plot_map(anom_mean,calc.lat,calc.lon,
+                 title='Anomalies Mean from Jan2000 to Dec2009 - El Nino Zone 4',
+                 savefig=1,name='mean_anomalies')
+        plot_map(anom,calc.lat,calc.lon,band=0,
+                 title='Potential Temperature Anomalie from Jan2000 - El Nino Zone 4',
+                 savefig=1,name='anom_jan2000en4')
+        plot_map(anom,calc.lat,calc.lon,band=24,
+                 title='Potential Temperature Anomalies from Jan2002 - El Nino Zone 4',
+                 savefig=1,name='anom_jan2002en4')
         calc.end_anom()
